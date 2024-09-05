7ea21d72851b0adf5d9e23cc8cf233dcf1b7f14e
Igor Novgorodov <igor.novgorodov@dfinity.org>
2024-09-05 20:23:31 +0200

    use strip_connection_headers in proxy()

--- src/http/proxy.rs
+++ src/http/proxy.rs
@@ -3,18 +3,23 @@ use std::sync::Arc;
 use axum::{body::Body, extract::Request, response::Response};
 use url::Url;
 
-use super::{body::SyncBodyDataStream, Client, Error};
+use super::{body::SyncBodyDataStream, headers::strip_connection_headers, Client, Error};
 
-/// Proxies provided Axum request to a given URL using Reqwest Client trait object and returns Axum response
+/// Proxies provided Axum request to a given URL using Client trait object and returns Axum response
 pub async fn proxy(
     url: Url,
     request: Request,
     http_client: &Arc<dyn Client>,
 ) -> Result<Response, Error> {
     // Convert Axum request into Reqwest one
-    let (parts, body) = request.into_parts();
+    let (mut parts, body) = request.into_parts();
+
+    // Strip connection-related headers so that the request fits HTTP/2
+    strip_connection_headers(&mut parts.headers);
+
     let mut request = reqwest::Request::new(parts.method.clone(), url);
     *request.headers_mut() = parts.headers;
+
     // Use SyncBodyDataStream wrapper that is Sync (Axum body is !Sync)
     *request.body_mut() = Some(reqwest::Body::wrap_stream(SyncBodyDataStream::new(body)));
 
@@ -27,7 +32,7 @@ pub async fn proxy(
         .status(response.status())
         .body(Body::from_stream(response.bytes_stream()))?;
 
-    // Copy the headers
+    // Assign the headers
     *response.headers_mut() = headers;
 
     Ok(response)
