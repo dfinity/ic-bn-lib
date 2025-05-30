8f2e9a82a057d6ff7db53a32c7e6bf21354ae728
Igor Novgorodov <igor.novgorodov@dfinity.org>
2025-05-30 17:23:02 +0200

    add proxy protocol

--- src/http/server/cli.rs
+++ src/http/server/cli.rs
@@ -5,6 +5,8 @@ use humantime::parse_duration;
 
 use crate::{parse_size, tls};
 
+use super::ProxyProtocolMode;
+
 #[derive(Args, Clone, Debug, Eq, PartialEq)]
 pub struct HttpServer {
     /// Backlog of incoming connections to set on the listening socket
@@ -61,11 +63,6 @@ pub struct HttpServer {
     #[clap(env, long, default_value = "10s", value_parser = parse_duration)]
     pub http_server_http2_keepalive_timeout: Duration,
 
-    /// How long to wait for the existing connections to finish before shutting down.
-    /// Also applies to the recycling of connections with `http_server_max_requests_per_conn` option.
-    #[clap(env, long, default_value = "60s", value_parser = parse_duration)]
-    pub http_server_grace_period: Duration,
-
     /// Maximum size of cache to store TLS sessions in memory
     #[clap(env, long, default_value = "256MB", value_parser = parse_size)]
     pub http_server_tls_session_cache_size: u64,
@@ -77,6 +74,19 @@ pub struct HttpServer {
     /// Lifetime of a TLS1.3 ticket, due to key rotation the actual lifetime will be twice than this
     #[clap(env, long, default_value = "9h", value_parser = parse_duration)]
     pub http_server_tls_ticket_lifetime: Duration,
+
+    /// How long to wait for the existing connections to finish before shutting down.
+    /// Also applies to the recycling of connections with `http_server_max_requests_per_conn` option.
+    #[clap(env, long, default_value = "60s", value_parser = parse_duration)]
+    pub http_server_grace_period: Duration,
+
+    /// Whether to expect connections with Proxy Protocol v2.
+    /// If the connection contains the Proxy Protocol v2 header - then we will the client's IP from it
+    /// instead of TCP endpoint.
+    /// Can be "off", "enabled" or "forced" - if "forced" then connections without a Proxy Protocol
+    /// header will not be accepted.
+    #[clap(env, long, default_value = "off")]
+    pub http_server_proxy_protocol_mode: ProxyProtocolMode,
 }
 
 impl From<&HttpServer> for super::Options {
@@ -93,6 +103,7 @@ impl From<&HttpServer> for super::Options {
             http2_max_streams: c.http_server_http2_max_streams,
             grace_period: c.http_server_grace_period,
             max_requests_per_conn: Some(c.http_server_max_requests_per_conn),
+            proxy_protocol_mode: c.http_server_proxy_protocol_mode,
         }
     }
 }
