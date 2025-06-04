bde6269b6dd68ab71c5b05d21251fdbf8caf79c9
Igor Novgorodov <igor.novgorodov@dfinity.org>
2025-06-04 20:03:40 +0200

    add PubSub module

---
+++ src/pubsub/mod.rs
@@ -0,0 +1,300 @@
+use std::{hash::Hash, marker::PhantomData, sync::Arc, time::Duration};
+
+use ahash::RandomState;
+use moka::sync::{Cache, CacheBuilder};
+use prometheus::{
+    IntCounter, IntGauge, Registry, register_int_counter_with_registry,
+    register_int_gauge_with_registry,
+};
+use tokio::sync::broadcast::{
+    Receiver, Sender,
+    error::{RecvError, SendError},
+};
+
+/// Trait that a topic ID should implement
+pub trait TopicId: Hash + Eq + Clone + Send + Sync + 'static {}
+impl<T: Hash + Eq + Clone + Send + Sync + 'static> TopicId for T {}
+
+/// Trait that a message should implement
+pub trait Message: Clone + Send + Sync + 'static {}
+impl<T: Clone + Send + Sync + 'static> Message for T {}
+
+/// Broker options
+#[derive(Clone, Debug)]
+pub struct Opts {
+    /// How many topics maximum we will support
+    pub max_topics: u64,
+    /// If the topic doesn't get messages for that long,
+    /// it will be deleted
+    pub idle_timeout: Duration,
+    /// Maximum buffer size of the publishing queue (per-topic).
+    /// When it's exceeded (due to slow consumers) - the slow consumers lose
+    /// the oldest messages
+    pub buffer_size: usize,
+}
+
+impl Default for Opts {
+    fn default() -> Self {
+        Self {
+            max_topics: 1_000_000,
+            idle_timeout: Duration::from_secs(600),
+            buffer_size: 10_000,
+        }
+    }
+}
+
+/// Topic to manage subscribers
+#[derive(Clone)]
+struct Topic<M: Message> {
+    tx: Sender<M>,
+}
+
+impl<M: Message> Topic<M> {
+    fn new(capacity: usize) -> Self {
+        Self {
+            tx: Sender::new(capacity),
+        }
+    }
+
+    fn subscribe(&self) -> Receiver<M> {
+        self.tx.subscribe()
+    }
+}
+
+/// Subscriber to receive messages
+pub struct Subscriber<M: Message> {
+    rx: Receiver<M>,
+    metric: IntGauge,
+}
+
+impl<M: Message> Subscriber<M> {
+    pub async fn recv(&mut self) -> Result<M, RecvError> {
+        self.rx.recv().await
+    }
+}
+
+impl<M: Message> Drop for Subscriber<M> {
+    fn drop(&mut self) {
+        // Decrement subscriber count
+        self.metric.dec();
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Metrics {
+    topics: IntGauge,
+    msgs_sent: IntCounter,
+    subscribers: IntGauge,
+}
+
+impl Metrics {
+    fn new(registry: &Registry) -> Self {
+        Self {
+            topics: register_int_gauge_with_registry!(
+                format!("pubsub_topics"),
+                format!("Number of topics currently active"),
+                registry
+            )
+            .unwrap(),
+
+            msgs_sent: register_int_counter_with_registry!(
+                format!("pubsub_msgs_sent"),
+                format!("Number of messages published"),
+                registry
+            )
+            .unwrap(),
+
+            subscribers: register_int_gauge_with_registry!(
+                format!("pubsub_subscribers"),
+                format!("Number of subscribers currently active"),
+                registry
+            )
+            .unwrap(),
+        }
+    }
+}
+
+pub struct Broker<M: Message, T: TopicId> {
+    opts: Opts,
+    topics: Cache<T, Arc<Topic<M>>, RandomState>,
+    metrics: Metrics,
+}
+
+impl<M: Message, T: TopicId> Broker<M, T> {
+    /// Create a new Broker
+    pub fn new(opts: Opts, registry: &Registry) -> Self {
+        let metrics = Metrics::new(registry);
+        let metrics_clone = metrics.clone();
+
+        let topics = CacheBuilder::new(opts.max_topics)
+            .eviction_listener(move |_k, _v, _r| {
+                metrics_clone.topics.dec();
+            })
+            .time_to_idle(opts.idle_timeout)
+            .build_with_hasher(RandomState::new());
+
+        Self {
+            opts,
+            topics,
+            metrics,
+        }
+    }
+
+    /// Fetches or creates a new topic
+    fn get_topic(&self, topic: &T) -> Arc<Topic<M>> {
+        self.topics.get_with_by_ref(topic, || {
+            self.metrics.topics.inc();
+            Arc::new(Topic::new(self.opts.buffer_size))
+        })
+    }
+
+    /// Subscribe to a given topic, returning a Receiver that can be used to consume messages
+    pub fn subscribe(&self, topic: &T) -> Subscriber<M> {
+        self.metrics.subscribers.inc();
+        let rx = self.get_topic(topic).subscribe();
+
+        Subscriber {
+            rx,
+            metric: self.metrics.subscribers.clone(),
+        }
+    }
+
+    /// Tries to send the message to the given topic.
+    /// The publish will fail if there's no subscribers.
+    pub fn publish(&self, topic: &T, message: M) -> Result<usize, SendError<M>> {
+        self.metrics.msgs_sent.inc();
+        self.get_topic(topic).tx.send(message)
+    }
+}
+
+pub struct BrokerBuilder<M, T> {
+    opts: Opts,
+    registry: Registry,
+    _m: PhantomData<M>,
+    _t: PhantomData<T>,
+}
+
+impl<M: Message, T: TopicId> Default for BrokerBuilder<M, T> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<M: Message, T: TopicId> BrokerBuilder<M, T> {
+    /// Create a new BrokerBuilder
+    pub fn new() -> Self {
+        Self {
+            opts: Opts::default(),
+            registry: Registry::new(),
+            _m: PhantomData,
+            _t: PhantomData,
+        }
+    }
+
+    /// Set the max number of topics supported. Default is 1 million.
+    pub const fn with_max_topics(mut self, max_topics: u64) -> Self {
+        self.opts.max_topics = max_topics;
+        self
+    }
+
+    /// Set the idle timeout when an inactive topic is removed. Default it 10min.
+    pub const fn with_idle_timeout(mut self, idle_timeout: Duration) -> Self {
+        self.opts.idle_timeout = idle_timeout;
+        self
+    }
+
+    /// Set per-topic buffer size. Default is 10k.
+    pub const fn with_buffer_size(mut self, buffer_size: usize) -> Self {
+        self.opts.buffer_size = buffer_size;
+        self
+    }
+
+    /// Set Prometheus registry to use
+    pub fn with_metric_registry(mut self, registry: &Registry) -> Self {
+        self.registry = registry.clone();
+        self
+    }
+
+    /// Build the Broker
+    pub fn build(self) -> Broker<M, T> {
+        Broker::new(self.opts, &self.registry)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_pubsub() {
+        let b: Broker<String, String> = BrokerBuilder::new().with_buffer_size(3).build();
+
+        let topic1 = "foo".to_string();
+        let topic2 = "dead".to_string();
+
+        // No subscribers - error
+        assert!(b.publish(&topic1, "".into()).is_err());
+        assert!(b.publish(&topic2, "".into()).is_err());
+        assert_eq!(b.metrics.topics.get(), 2);
+
+        // Subscribe
+        let mut foo_sub = b.subscribe(&topic1);
+        let mut dead_sub = b.subscribe(&topic2);
+        assert_eq!(b.metrics.subscribers.get(), 2);
+
+        // Publish up to a buffer size & receive
+        assert!(b.publish(&topic1, "bar1".into()).is_ok());
+        assert!(b.publish(&topic2, "beef1".into()).is_ok());
+        assert!(b.publish(&topic1, "bar2".into()).is_ok());
+        assert!(b.publish(&topic2, "beef2".into()).is_ok());
+        assert!(b.publish(&topic1, "bar3".into()).is_ok());
+        assert!(b.publish(&topic2, "beef3".into()).is_ok());
+        assert_eq!(b.metrics.msgs_sent.get(), 8);
+
+        assert_eq!(foo_sub.recv().await.unwrap(), "bar1");
+        assert_eq!(dead_sub.recv().await.unwrap(), "beef1");
+        assert_eq!(foo_sub.recv().await.unwrap(), "bar2");
+        assert_eq!(dead_sub.recv().await.unwrap(), "beef2");
+        assert_eq!(foo_sub.recv().await.unwrap(), "bar3");
+        assert_eq!(dead_sub.recv().await.unwrap(), "beef3");
+
+        // Publish more than a buffer size.
+        // The oldest message is lost.
+        assert!(b.publish(&topic1, "bar1".into()).is_ok());
+        assert!(b.publish(&topic2, "beef1".into()).is_ok());
+        assert!(b.publish(&topic1, "bar2".into()).is_ok());
+        assert!(b.publish(&topic2, "beef2".into()).is_ok());
+        assert!(b.publish(&topic1, "bar3".into()).is_ok());
+        assert!(b.publish(&topic2, "beef3".into()).is_ok());
+        assert!(b.publish(&topic1, "bar4".into()).is_ok());
+        assert!(b.publish(&topic2, "beef4".into()).is_ok());
+        assert!(b.publish(&topic1, "bar5".into()).is_ok());
+        assert!(b.publish(&topic2, "beef5".into()).is_ok());
+
+        assert!(matches!(
+            foo_sub.recv().await.unwrap_err(),
+            RecvError::Lagged(_)
+        ));
+        assert!(matches!(
+            dead_sub.recv().await.unwrap_err(),
+            RecvError::Lagged(_)
+        ));
+        assert_eq!(foo_sub.recv().await.unwrap(), "bar2");
+        assert_eq!(dead_sub.recv().await.unwrap(), "beef2");
+        assert_eq!(foo_sub.recv().await.unwrap(), "bar3");
+        assert_eq!(dead_sub.recv().await.unwrap(), "beef3");
+        assert_eq!(foo_sub.recv().await.unwrap(), "bar4");
+        assert_eq!(dead_sub.recv().await.unwrap(), "beef4");
+        assert_eq!(foo_sub.recv().await.unwrap(), "bar5");
+        assert_eq!(dead_sub.recv().await.unwrap(), "beef5");
+
+        // Drop subscribers
+        drop(foo_sub);
+        drop(dead_sub);
+        assert_eq!(b.metrics.subscribers.get(), 0);
+
+        // Again no subscribers - error
+        assert!(b.publish(&topic1, "".into()).is_err());
+        assert!(b.publish(&topic2, "".into()).is_err())
+    }
+}
