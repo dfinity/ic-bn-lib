60b9f17dafd87a8747bf9df0edf3a2e789ef0099
Igor Novgorodov <igor@novg.net>
2025-06-27 12:43:38 +0200

    fix clippy

--- src/tls/mod.rs
+++ src/tls/mod.rs
@@ -280,6 +280,7 @@ mod test {
     use super::*;
 
     #[test]
+    #[allow(clippy::cognitive_complexity)]
     fn test_sni_matches() {
         let domains = vec![fqdn!("foo1.bar"), fqdn!("foo2.bar"), fqdn!("foo3.bar")];
 
