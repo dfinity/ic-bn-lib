df55aadf35cae38e5bc60febfc3170d448249b98
Igor Novgorodov <igor@novg.net>
2025-07-18 14:04:46 +0200

    improve distributor metrics

--- src/utils/distributor.rs
+++ src/utils/distributor.rs
@@ -2,7 +2,7 @@ use std::{
     fmt::{Debug, Display},
     sync::{
         Arc, Mutex,
-        atomic::{AtomicUsize, Ordering},
+        atomic::{AtomicBool, AtomicUsize, Ordering},
     },
     time::Instant,
 };
@@ -225,26 +225,29 @@ where
             .with_label_values(&[&backend.name])
             .inc();
 
+        let start = Instant::now();
+        let ok = Arc::new(AtomicBool::new(false));
+        let ok_clone = ok.clone();
+
+        // Record metrics under defer to make sure they're recorded in case of future cancellation
         defer! {
             backend.inflight.fetch_sub(1, Ordering::SeqCst);
             self.metrics.inflight.with_label_values(&[&backend.name]).dec();
+            self.metrics
+                .duration
+                .with_label_values(&[&backend.name])
+                .observe(start.elapsed().as_secs_f64());
+            self.metrics
+                .requests
+                .with_label_values(&[
+                    backend.name.as_str(),
+                    if ok_clone.load(Ordering::SeqCst) { "ok" } else { "fail" },
+                ])
+                .inc();
         }
 
-        let start = Instant::now();
         let res = self.executor.execute(&backend.backend, request).await;
-        self.metrics
-            .duration
-            .with_label_values(&[&backend.name])
-            .observe(start.elapsed().as_secs_f64());
-
-        self.metrics
-            .requests
-            .with_label_values(&[
-                backend.name.as_str(),
-                if res.is_ok() { "ok" } else { "fail" },
-            ])
-            .inc();
-
+        ok.store(res.is_ok(), Ordering::SeqCst);
         res
     }
 }
