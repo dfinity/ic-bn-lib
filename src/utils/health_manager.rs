3d240b4a1c805e5e261f1da5b414d6f3084204f8
Igor Novgorodov <igor@novg.net>
2025-08-28 20:18:25 +0200

    Make HealthManager Sync

--- src/utils/health_manager.rs
+++ src/utils/health_manager.rs
@@ -1,4 +1,4 @@
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 
 use crate::types::Healthy;
 
@@ -6,17 +6,17 @@ use crate::types::Healthy;
 /// It is healthy when all inner services are healthy.
 #[derive(Debug, Default)]
 pub struct HealthManager {
-    services: Vec<Arc<dyn Healthy>>,
+    services: Mutex<Vec<Arc<dyn Healthy>>>,
 }
 
 impl HealthManager {
-    pub fn add(&mut self, svc: Arc<dyn Healthy>) {
-        self.services.push(svc);
+    pub fn add(&self, svc: Arc<dyn Healthy>) {
+        self.services.lock().unwrap().push(svc);
     }
 }
 
 impl Healthy for HealthManager {
     fn healthy(&self) -> bool {
-        self.services.iter().all(|x| x.healthy())
+        self.services.lock().unwrap().iter().all(|x| x.healthy())
     }
 }
