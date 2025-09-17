3268fb8d7553e16568f2bcdfdd8317fdecab83d8
Igor Novgorodov <igor@novg.net>
2025-09-17 10:57:52 +0200

    add dyn_event

--- src/lib.rs
+++ src/lib.rs
@@ -183,3 +183,16 @@ macro_rules! retry_async {
         retry_async!($f, Duration::from_secs(60), Duration::from_millis(500))
     };
 }
+
+#[macro_export]
+macro_rules! dyn_event {
+    ($lvl:ident, $($arg:tt)+) => {
+        match $lvl {
+            ::tracing::Level::TRACE => ::tracing::trace!($($arg)+),
+            ::tracing::Level::DEBUG => ::tracing::debug!($($arg)+),
+            ::tracing::Level::INFO => ::tracing::info!($($arg)+),
+            ::tracing::Level::WARN => ::tracing::warn!($($arg)+),
+            ::tracing::Level::ERROR => ::tracing::error!($($arg)+),
+        }
+    };
+}
