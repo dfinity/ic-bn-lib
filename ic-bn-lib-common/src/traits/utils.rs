2ab3e0998b7ffddc7a01bb8fca688fed2fbe0afc
Igor Novgorodov <igor@novg.net>
2025-11-06 20:23:43 +0100

    Move SevSnpCli

--- ic-bn-lib-common/src/traits/utils.rs
+++ ic-bn-lib-common/src/traits/utils.rs
@@ -1,13 +1,8 @@
-use std::{
-    fmt::{Debug, Display},
-    time::Duration,
-};
+use std::fmt::{Debug, Display};
 
 use async_trait::async_trait;
-use clap::Args;
-use humantime::parse_duration;
 
-use crate::{parse_size, types::utils::TargetState};
+use crate::types::utils::TargetState;
 
 /// Trait that executes the requests.
 /// Akin to Tower's Service, but generic over backend.
@@ -25,19 +20,4 @@ pub trait ExecutesRequest<T>: Send + Sync + Debug {
 #[async_trait]
 pub trait ChecksTarget<T: Clone + Display + Debug>: Send + Sync + 'static {
     async fn check(&self, target: &T) -> TargetState;
-}
-
-#[derive(Args)]
-pub struct SevSnpCli {
-    /// Enable SEV-SNP measurement reporting
-    #[clap(env, long)]
-    pub sev_snp_enable: bool,
-
-    /// Cache TTL for SEV-SNP reports
-    #[clap(env, long, default_value = "30s", value_parser = parse_duration)]
-    pub sev_snp_cache_ttl: Duration,
-
-    /// Max cache size for SEV-SNP reports
-    #[clap(env, long, default_value = "10m", value_parser = parse_size)]
-    pub sev_snp_cache_size: u64,
 }
