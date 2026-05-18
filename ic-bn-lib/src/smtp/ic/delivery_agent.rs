197f88a7999eb8b60960186a66e92fdd05230b5f
Igor Novgorodov <igor@novg.net>
2026-05-18 20:07:27 +0200

    Add lookup test

--- ic-bn-lib/src/smtp/ic/delivery_agent.rs
+++ ic-bn-lib/src/smtp/ic/delivery_agent.rs
@@ -3,6 +3,7 @@ use std::{fmt::Display, str::FromStr, sync::Arc};
 use anyhow::Context;
 use async_trait::async_trait;
 use candid::{Decode, Encode, Principal};
+use derive_new::new;
 use ic_agent::Agent;
 
 use crate::{
@@ -19,13 +20,11 @@ use crate::{
 pub enum IcSmtpDeliveryAgentError {
     #[error("IC Agent error: {0}")]
     Agent(#[from] ic_agent::AgentError),
-    #[error("Candid error: {0}")]
-    Candid(#[from] candid::Error),
     #[error("{0}")]
     Other(#[from] anyhow::Error),
 }
 
-#[derive(Debug)]
+#[derive(Debug, new)]
 pub struct IcSmtpDeliveryAgent {
     agent: Agent,
     custom_domains: Arc<dyn LooksupCustomDomain>,
@@ -59,7 +58,7 @@ impl IcSmtpDeliveryAgent {
         request: SmtpRequest,
         validate: bool,
     ) -> Result<SmtpResponse, IcSmtpDeliveryAgentError> {
-        let arg = Encode!(&request)?;
+        let arg = Encode!(&request).context("unable to encode SMTP request")?;
 
         let resp = if validate {
             self.agent
@@ -75,7 +74,7 @@ impl IcSmtpDeliveryAgent {
                 .await?
         };
 
-        let resp = Decode!(&resp, SmtpResponse)?;
+        let resp = Decode!(&resp, SmtpResponse).context("unable to decode SMTP response")?;
         Ok(resp)
     }
 }
@@ -94,5 +93,68 @@ impl ResolvesRecipient for IcSmtpDeliveryAgent {
         rcpt: &EmailAddress,
     ) -> Result<RecipientPolicy, RecipientResolveError> {
         Ok(RecipientPolicy::Accept)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ahash::HashMap;
+    use fqdn::{FQDN, fqdn};
+    use ic_bn_lib_common::{principal, types::CustomDomain};
+
+    #[derive(Debug)]
+    struct TestDomainResolver(HashMap<FQDN, CustomDomain>);
+
+    impl LooksupCustomDomain for TestDomainResolver {
+        fn lookup_custom_domain(&self, hostname: &fqdn::Fqdn) -> Option<CustomDomain> {
+            self.0.get(hostname).cloned()
+        }
+    }
+
+    #[test]
+    fn test_canister_resolver() {
+        let resolver = TestDomainResolver(HashMap::from_iter([
+            (
+                fqdn!("foo.bar"),
+                CustomDomain {
+                    name: fqdn!("foo.bar"),
+                    canister_id: principal!("qoctq-giaaa-aaaaa-aaaea-cai"),
+                    timestamp: 0,
+                },
+            ),
+            (
+                fqdn!("dead.beef"),
+                CustomDomain {
+                    name: fqdn!("dead.beef"),
+                    canister_id: principal!("uqzsh-gqaaa-aaaaq-qaada-cai"),
+                    timestamp: 0,
+                },
+            ),
+        ]));
+
+        let ic_agent = Agent::builder().with_url("http://foo").build().unwrap();
+        let delivery_agent = IcSmtpDeliveryAgent::new(ic_agent, Arc::new(resolver));
+
+        for (email, canister_id) in [
+            (
+                "foo@foo.bar",
+                Some(principal!("qoctq-giaaa-aaaaa-aaaea-cai")),
+            ),
+            (
+                "foo@dead.beef",
+                Some(principal!("uqzsh-gqaaa-aaaaq-qaada-cai")),
+            ),
+            (
+                "foo@lusdn-iiaaa-aaaam-qivpa-cai.icp0.io",
+                Some(principal!("lusdn-iiaaa-aaaam-qivpa-cai")),
+            ),
+            ("foo@some-random-domain.org", None),
+        ] {
+            assert_eq!(
+                delivery_agent.resolve_canister(&EmailAddress::from_str(email).unwrap()),
+                canister_id
+            );
+        }
     }
 }
